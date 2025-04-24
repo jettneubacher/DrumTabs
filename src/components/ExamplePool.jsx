c0324d1d24fa4e17a4031d95496bfedad31762d6
jettneubacher <jettneubacher9@gmail.com>
2025-04-24 12:10:02 -0400

    example filepaths resolve 4

--- src/components/ExamplePool.jsx
+++ src/components/ExamplePool.jsx
@@ -5,17 +5,17 @@ const examples = {
   model1: [
     {
       label: "Example 1",
-      src: "https://jettneubacher.github.io/DrumTabs/public/example-audio/example-1.1.wav",
+      src: "https://github.com/jettneubacher/DrumTabs/raw/refs/heads/main/public/example-audio/example-1.1.wav",
       filename: "example-1.1.wav",
     },
     {
       label: "Example 2",
-      src: "https://jettneubacher.github.io/DrumTabs/public/example-audio/example-1.2.wav",
+      src: "https://github.com/jettneubacher/DrumTabs/raw/refs/heads/main/public/example-audio/example-1.2.wav",
       filename: "example-1.2.wav",
     },
     {
       label: "Example 3",
-      src: "https://jettneubacher.github.io/DrumTabs/public/example-audio/example-1.3.wav",
+      src: "https://github.com/jettneubacher/DrumTabs/raw/refs/heads/main/public/example-audio/example-1.3.wav",
       filename: "example-1.3.wav",
     },
   ],
